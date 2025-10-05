b3a7cd3a35ce352d6af7f24540d48ee912b7372c
Roksana Dilshad <tanvirhossaintufa@gmail.com>
2025-10-05 12:45:30 +0600

    add navbar and footer

---
+++ book-poka/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='grid place-content-center pt-[22%]'>
+            <div className="text-4xl primary ">
+            <h1>404 PAGE NOT FOUND</h1>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
